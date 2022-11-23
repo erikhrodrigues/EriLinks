a2f3784175d583ca146655c64ce8bf3a8465d353
Erik Henrique <erikhrodrigues@gmail.com>
2022-11-23 06:06:26 -0300

    finalizando projeto

--- js/main.js
+++ js/main.js
@@ -5,42 +5,47 @@ let resultado = document.querySelector("#res");
 btn.addEventListener("click", encurtarLink);
 
 async function encurtarLink() {
-    console.log(link.value)
     try{
         let api = await fetch(`https://api.shrtco.de/v2/shorten?url=${link.value}`).then(response => response.json());
         let apiConvertida = api.result
         console.log(apiConvertida)
         imprimirResultado(link.value, apiConvertida.full_short_link);
+
         
     } catch(erro) {
-        alert("Aconteceu algum erro com o link, verifique novamente!")
+        alert("Infelizmente esse link não pode ser encurtado!")
+    } finally {
+        link.value = ``
     }
 }
 
+
 function imprimirResultado(linkGrande, linkCurto){
+    
     let div = document.createElement("div");
     div.classList.add("resultado");
     resultado.appendChild(div);
 
     let resultadoAntigo = document.createElement("p");
     resultadoAntigo.classList.add("resultado__antigo");
-    resultadoAntigo.innerHTML = `${linkGrande}`;
+    resultadoAntigo.innerHTML = `${linkGrande.substr(0, 40)}...`;
     div.appendChild(resultadoAntigo);
 
     let resultadoNovo = document.createElement("a");
     resultadoNovo.classList.add("resultado__novo");
     resultadoNovo.setAttribute("href", `${linkCurto}`);
+    resultadoNovo.setAttribute("target", `_blank`);
     resultadoNovo.textContent = `${linkCurto}`
     div.appendChild(resultadoNovo);
 
 
     let botaoDeCopiar = document.createElement("button");
     botaoDeCopiar.classList.add("resultado__btn");
-    botaoDeCopiar.innerHTML = `Copy`;
+    botaoDeCopiar.innerHTML = `Copiar`;
     botaoDeCopiar.addEventListener("click", () => {
         navigator.clipboard.writeText(resultadoNovo);
-        botaoDeCopiar.innerHTML = `Copied!`
-        botaoDeCopiar.classList.toggle("copied")
+        botaoDeCopiar.innerHTML = `Copiado!`
+        botaoDeCopiar.classList.add("copied")
     });
     div.appendChild(botaoDeCopiar);
 
